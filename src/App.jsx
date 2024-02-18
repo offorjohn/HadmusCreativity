3f9f6f7d18245f1f5e824cec608bd2b84c984cf9
offor <offorchukwuebuka8@gmail.com>
2024-02-18 23:04:45 +0100

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from "./pages/HomePage/HomePage";
+import AuthPage from "./pages/AuthPage/AuthPage";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/auth' element={<AuthPage />} />
 
       </Routes>
     </>
