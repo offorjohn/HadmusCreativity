73475494e380c69c49d18822628ea0cb2c3fb8bc
offor <offorchukwuebuka8@gmail.com>
2024-02-19 21:02:07 +0100

    Your commit message

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,40 +1,48 @@
-import { Box, Button, Image, Input, VStack } from '@chakra-ui/react';
+import { Box, Button, Image, Input, VStack, Flex, Text } from '@chakra-ui/react'; // Import Flex and Text components
 import { useState } from 'react';
 
 const AuthForm = () => {
-    const [isLogin, setIsLogin] = useState(true)
-    return <>
-        <Box border={"1px solid gray"} borderRadius={4} padding={5}>
-            <VStack spacing={4}>
-                <Image src='/logo.png' h={24} cursor={"pointer"} alt='Instagram' />
-                <input
-                    placeholder='Email'
-                    fontSize={14}
-                    type='email'
-                />
-                <input
-                    placeholder='Password'
-                    fontSize={14}
-                    type='password'
-                />
+  const [isLogin, setIsLogin] = useState(true);
 
-                {!isLogin ? (
-                    <Input
-                        placeholder='Confirm Password'
-                        fontSize={14}
-                        type='password'
-                    />
+  return (
+    <Box border="1px solid gray" borderRadius={4} padding={5}>
+      <VStack spacing={4} align="center"> {/* Specify align prop for VStack */}
+        <Image src="/logo.png" h={24} cursor="pointer" alt="Instagram" />
+        <Input
+          placeholder="Email"
+          fontSize={14}
+          type="email"
+        />
+        <Input
+          placeholder="Password"
+          fontSize={14}
+          type="password" 
+        />
 
-                ) : null}
+        {!isLogin ? (
+          <Input
+            placeholder="Confirm Password"
+            fontSize={14}
+            type="password"
+          />
+        ) : null}
 
-                <Button w={"full"} colorScheme='blue' size={"sm"} fontSize={14}>
-                    {isLogin? "Login" : "Sign Up"}
-                </Button>
+        <Button w="full" colorScheme="blue" size="sm" fontSize={14}>
+          {isLogin ? "Login" : "Sign Up"}
+        </Button>
 
-            </VStack>
+        {/* ----------------OR TEXT -------- */}
 
-        </Box>
-    </>;
+        <Flex alignItems="center" justifyContent="center" my={4} gap={1} w="full">
+          <Box flex={2} h="1px" bg="gray.400" />
+          <Text mx={1} color="white">
+            OR
+          </Text>
+          <Box flex={2} h="1px" bg="gray.400" />
+        </Flex>
+      </VStack>
+    </Box>
+  );
 };
 
-export default AuthForm
+export default AuthForm;
