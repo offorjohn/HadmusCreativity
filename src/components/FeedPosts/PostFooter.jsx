97c781da4858ccd33f3490f30b426ae4d879fa6f
offor <offorchukwuebuka8@gmail.com>
2024-02-22 15:38:49 +0100

    Your commit message

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,10 +1,27 @@
-
+import { Box, Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import { NotificationsLogo, UnlikeLogo } from "../../assets/constacts";
 
 const PostFooter = () => {
-  return <>
-  </>;
-  
-  
-}
+    const [liked, setLiked] = useState(false);
+    const [likes, setLikes] = useState(1000);
 
-export default PostFooter
+    const handleLike = () => {
+        if (liked) {
+            setLiked(false);
+            setLikes(likes - 1);
+        } else {
+            setLiked(true);
+        }
+    };
+
+    return (
+        <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={"auto"}>
+            <Box onClick={handleLike}>
+                {!liked ? <NotificationsLogo /> : <UnlikeLogo />}
+            </Box>
+        </Flex>
+    );
+};
+
+export default PostFooter;
