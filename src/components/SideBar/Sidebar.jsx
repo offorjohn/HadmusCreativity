35facf4d614981ad366ac77595cda7e032ac5e6c
offor <offorchukwuebuka8@gmail.com>
2024-02-22 12:45:31 +0100

    Your commit message

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -4,6 +4,7 @@ import { CreatePostLogo, InstagramLogo, InstagramMobileLogo, NotificationsLogo, 
 
 
 import { AiFillHome } from "react-icons/ai";
+import { BiLogOut } from "react-icons/bi";
 
 const Sidebar = () => {
     const sidebarItems = [
@@ -74,19 +75,19 @@ const Sidebar = () => {
                     >
                         <Link
                             display={"flex"}
-                            to={ item.link || null}
-                            as={RouterLink} 
+                            to={item.link || null}
+                            as={RouterLink}
                             alignItems={"center"}
                             gap={4}
                             _hover={{ bg: "whiteAlpha.400" }}
                             borderRadius={6}
                             p={2}
                             w={{ base: 10, md: "full" }}
-                            justifyContent={{base: "center", md:"flex-start"}}
+                            justifyContent={{ base: "center", md: "flex-start" }}
 
                         >
                             {item.icon}
-                            <Box display={{base:"none",md:"block"}}>
+                            <Box display={{ base: "none", md: "block" }}>
                                 {item.text}
 
                             </Box>
@@ -96,6 +97,38 @@ const Sidebar = () => {
                 ))}
 
             </Flex>
+
+            <Tooltip
+
+                hasArrow
+                label={"Logout"}
+                placement="right"
+                ml={1}
+                openDelay={500}
+                display={{ base: 'block', md: "none" }}
+            >
+                <Link
+                    display={"flex"}
+                    to={"/auth"}
+                    as={RouterLink}
+                    alignItems={"center"}
+                    gap={4}
+                    _hover={{ bg: "whiteAlpha.400" }}
+                    borderRadius={6}
+                    p={2}
+                    w={{ base: 10, md: "full" }}
+                    mt={'auto'}
+                    justifyContent={{ base: "center", md: "flex-start" }}
+
+                >
+                    <BiLogOut size={25} />
+                    <Box display={{ base: "none", md: "block" }}>Logout
+                        
+
+                    </Box>
+                </Link>
+
+            </Tooltip>
         </Flex>
 
     </Box>;
