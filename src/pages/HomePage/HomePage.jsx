6f0362d4e9ae1d5ca3452e8149b0b0176412737a
offor <offorchukwuebuka8@gmail.com>
2024-02-22 13:28:19 +0100

    Your commit message

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,21 @@
+import { Box, Container, Flex } from "@chakra-ui/react";
+
 
 
  const HomePage = () => {
-  return <div>Homepage</div>;
+  return (
+    <Container maxW={"container.lg"}>
+      <Flex gap={20}>
+        <Box flex={2} py={10} border={"1px solid blue"}>
+           FeedPosts
+           </Box>
+        <Box flex={2} mr={20} display={{ base: "none", lg: "block" }} maxW={"300px"} border={"1x solid red"}>
+          Suggested Users
+        </Box>
+       </Flex>
+      </Container>  
+  )
 
-}
+};
 
 export default HomePage
