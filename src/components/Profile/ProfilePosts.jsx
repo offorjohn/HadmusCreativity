adf54e708d32b4724d3fcea2d79f60a0c5f02099
offor <offorchukwuebuka8@gmail.com>
2024-02-24 23:53:04 +0100

    Your commit message

--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,9 +1,44 @@
-
+import { Box, Grid, Skeleton, VStack } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import ProfilePost from './ProfilePost'; // Make sure to adjust the path based on your project structure
 
 const ProfilePosts = () => {
-  return (
-    <div>ProfilePosts</div>
-  )
-}
+  const [isLoading, setIsLoading] = useState(true);
 
-export default ProfilePosts
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <Grid
+      templateColumns={{
+        sm: "repeat(1, 1fr)",
+        md: "repeat(3, 1fr)",
+      }}
+      gap={1}
+      columnGap={1}
+    >
+      {isLoading &&
+        [0, 1, 2, 3, 5].map((_, idx) => (
+          <VStack key={idx} alignItems="flex-start" gap={4}>
+            <Skeleton w="full">
+              <Box h='300px'>contents wrapped</Box>
+            </Skeleton>
+          </VStack>
+        ))}
+
+      {!isLoading && (
+        <>
+          <ProfilePost img="/img1.png" />
+          <ProfilePost img="/img2.png" />
+          <ProfilePost img="/img3.png" />
+          <ProfilePost img="/img4.png" />
+        </>
+      )}
+    </Grid>
+  );
+};
+
+export default ProfilePosts;
