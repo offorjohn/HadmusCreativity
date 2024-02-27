37917947b719071746ebb9ed122c30b4c938c96c
offor <offorchukwuebuka8@gmail.com>
2024-02-27 19:11:56 +0100

    Your commit message

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,32 +1,45 @@
-import { Box, Flex } from "@chakra-ui/react";
-//import Sidebar from "../../components/Sidebar/Sidebar";
+import { Box, Flex, Spinner } from "@chakra-ui/react";
 import { useLocation } from "react-router-dom";
-import Sidebar from "../../components/SideBar/Sidebar";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/firebase";
-
+import Navbar from "../../components/Navbar/Navbar";
+import Sidebar from "../../components/SideBar/Sidebar";
 
 const PageLayout = ({ children }) => {
     const { pathname } = useLocation();
-    const [user, loading, error] = useAuthState(auth);
+    const [user, loading] = useAuthState(auth);
     const canRenderSidebar = pathname !== "/auth" && user;
+    const canRenderNavbar = !user && pathname !== "/auth";
+
+    const checkingUserIsAuth = !user && loading;
+    if (checkingUserIsAuth) return <PageLayoutSpinner />;
 
     return (
-        <Flex>
+        <Flex flexDir={canRenderNavbar ? "column" : "row"}>
             {/* sidebar on the left */}
             {canRenderSidebar ? (
                 <Box w={{ base: "78px", md: "240px" }}>
                     <Sidebar />
                 </Box>
             ) : null}
+
+            {/* Navbar */}
+            {canRenderNavbar && <Navbar />}
+            
             {/* the page content on the right */}
-            <Box flex={1} w={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)" }}>
+            <Box flex={1} w={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)" }} mx={"auto"}>
                 {children}
             </Box>
-
         </Flex>
-
     );
 };
 
 export default PageLayout;
+
+const PageLayoutSpinner = () => {
+    return (
+        <Flex flexDir='column' h='100vh' alignItems='center' justifyContent='center'>
+            <Spinner size='xl' />
+        </Flex>
+    );
+};
--- src/hooks/useSignUpWithEmailAndPassword.js
+++ src/hooks/useSignUpWithEmailAndPassword.js
@@ -1,33 +1,41 @@
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { auth, firestore } from '../firebase/firebase';
-import { doc, setDoc } from 'firebase/firestore';
+import { collection, doc, query, where, getDocs, setDoc } from 'firebase/firestore'; // Added missing imports
 import useShowToast from './useShowToast';
 import useAuthStore from '../store/authStore';
 
 const useSignUpWithEmailAndPassword = () => {
     const [
         createUserWithEmailAndPassword,
-        user, // Include 'user' in the destructuring assignment
+        user,
         loading,
         error,
     ] = useCreateUserWithEmailAndPassword(auth);
 
     const showToast = useShowToast();
-    const loginUser = useAuthStore(state => state.login)
-    
+    const loginUser = useAuthStore(state => state.login);
 
     const signup = async (inputs) => {
         try {
             if (!inputs.email || !inputs.password || !inputs.username || !inputs.fullName) {
                 showToast("Error", "Please fill all the fields", "error");
-                return; // Return to avoid further execution
+                return;
+            }
+
+            const usersRef = collection(firestore, "users");
+            const q = query(usersRef, where("username", "==", inputs.username)); // Fixed where condition
+            const querySnapshot = await getDocs(q);
+
+            if (!querySnapshot.empty) {
+                showToast("Error", "Username already exists", "error");
+                return;
             }
 
             const newUserCredential = await createUserWithEmailAndPassword(inputs.email, inputs.password);
 
             if (!newUserCredential || error) {
                 showToast("Error", error?.message || "Unknown error", "error");
-                return; // Return to avoid further execution
+                return;
             }
 
             const { user: newUser } = newUserCredential;
@@ -47,16 +55,12 @@ const useSignUpWithEmailAndPassword = () => {
 
             await setDoc(doc(firestore, "users", newUser.uid), userDoc);
             localStorage.setItem("user-info", JSON.stringify(userDoc));
-            loginUser(userDoc)
-
-
+            loginUser(userDoc);
         } catch (error) {
             showToast("Error", error?.message || "Unknown error", "error");
-            // Handle the error in your UI or log it as needed.
         }
     };
 
-    // Explicitly return values from the hook
     return {
         signup,
         user,
