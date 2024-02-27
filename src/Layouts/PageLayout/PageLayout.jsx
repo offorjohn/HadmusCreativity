2b85281534ae247834fdacc4ce30c3cb487d3f32
offor <offorchukwuebuka8@gmail.com>
2024-02-27 17:54:33 +0100

    Your commit message

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -2,14 +2,19 @@ import { Box, Flex } from "@chakra-ui/react";
 //import Sidebar from "../../components/Sidebar/Sidebar";
 import { useLocation } from "react-router-dom";
 import Sidebar from "../../components/SideBar/Sidebar";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase/firebase";
+
 
 const PageLayout = ({ children }) => {
     const { pathname } = useLocation();
+    const [user, loading, error] = useAuthState(auth);
+    const canRenderSidebar = pathname !== "/auth" && user;
 
     return (
         <Flex>
             {/* sidebar on the left */}
-            {pathname !== "/auth" ? (
+            {canRenderSidebar ? (
                 <Box w={{ base: "78px", md: "240px" }}>
                     <Sidebar />
                 </Box>
